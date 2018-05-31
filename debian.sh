1534336fa4ef672421fc944f29cc32b6e07e5561
sslmode <34929896+sslmode@users.noreply.github.com>
2018-06-01 01:29:05 +0800

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -101,15 +101,34 @@ sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php
 service php5-fpm restart
 service nginx restart
 
+# install openvpn
+wget -O /etc/openvpn/openvpn.tar "https://github.com/Mbah-Shondong/Debian732/raw/master/Debian7/openvpn-debian.tar"
+cd /etc/openvpn/
+tar xf openvpn.tar
+wget -O /etc/openvpn/1194.conf "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/1194.conf"
+service openvpn restart
+sysctl -w net.ipv4.ip_forward=1
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
+iptables-save > /etc/iptables_yg_baru_dibikin.conf
+wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/iptables"
+chmod +x /etc/network/if-up.d/iptables
+service openvpn restart
+
+#konfigurasi openvpn
+cd /etc/openvpn/
+wget -O /etc/openvpn/client.ovpn "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/client-1194.conf"
+sed -i $MYIP2 /etc/openvpn/client.ovpn;
+cp client.ovpn /home/vps/public_html/
 
 # install badvpn
-#wget -O /usr/bin/badvpn-udpgw $source/badvpn-udpgw
-#if [[ $OS == "x86_64" ]]; then
-#wget -O /usr/bin/badvpn-udpgw $source/badvpn-udpgw64"
-#fi
-#sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
-#chmod +x /usr/bin/badvpn-udpgw
-#screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
+wget -O /usr/bin/badvpn-udpgw $source/badvpn-udpgw
+if [[ $OS == "x86_64" ]]; then
+wget -O /usr/bin/badvpn-udpgw $source/badvpn-udpgw64"
+fi
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
+chmod +x /usr/bin/badvpn-udpgw
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
 cd
 
 # Install Mrtg
